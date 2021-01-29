605f0b3c89dbc8a8176b590b78ebd6b8c21e4774
srinivasraosamudrala <61248861+srinivasraosamudrala@users.noreply.github.com>
2021-01-29 11:21:07 -0800

    Changes

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,26 @@
-// import express module
 const express = require('express');
-// create  an express app
 const app = express();
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
-//Allow Access Control
 app.use(function (req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://18.219.36.140:8080');
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    res.setHeader('Cache-Control', 'no-cache');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    // res.setHeader('Cache-Control', 'no-cache');
     next();
 });
-// app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/submitForm', (req, res) => {
-    console.log(req.body)
+app.post('/registration', (req, res) => {
+    // console.log(req.body)
     res.status(200).send({
-        success: true,
-        status: "Name successfully sent: " + req.body.name
+        users: req.body.username,
+        count:req.body.count+1
     })
 });
 
---
+++ frontend/script/app.module.js
@@ -0,0 +1,30 @@
+angular.module("app", [])
+    .controller('registrationController', ['$scope', '$http', function ($scope, $http) {
+        $scope.username = "";
+        $scope.users = "";
+        $scope.count = 0;
+        $scope.error = "";
+        
+        $scope.Registration = () => {
+            if($scope.username == "" || $scope.username == $scope.users){
+                $scope.error = "Enter proper name"
+            }else{
+            $scope.data = {
+                username: $scope.username,
+                count:$scope.count
+            }
+            $http({
+                method: 'POST',
+                url: 'http://127.0.0.1:3001/registration',
+                data: $scope.data,
+                headers: { 'Content-Type': 'application/json' }
+            }).then((response) => {
+                $scope.users = response.data.users;
+                $scope.count = response.data.count;
+            }, (response) => {
+                $scope.error = response.statusText;
+            });
+            $scope.error = ""
+        };
+        }
+    }]);
